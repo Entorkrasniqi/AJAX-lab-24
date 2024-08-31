f178ddbd6f1e4b43cfdcfb2ee14ae77f2247e42b
Entorkrasniqi <124073124+Entorkrasniqi@users.noreply.github.com>
2024-08-31 16:05:11 +0300

    update

---
+++ t4/t4.js
@@ -0,0 +1,24 @@
+async function haeDataa() {
+  try {
+    const user = {
+      name: 'John Doe',
+      job: 'Developer',
+    };
+    const url = 'https://reqres.in/api/users';
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(user),
+    };
+
+    // eslint-disable-next-line no-undef
+    const userData = await fetchData(url, options);
+    console.log(userData);
+  } catch (error) {
+    alert(error.message);
+  }
+}
+
+haeDataa();
